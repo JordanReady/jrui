6e99cfdf382d3da71b6ced2aecfe9d4dcbcbf05e
JordanReady <jready112@gmail.com>
2024-03-04 23:21:26 -0600

    add: cumming soon

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container flex align-middle justify-center mx-auto p-8">
+    <div className="container flex flex-col text-center align-middle justify-center mx-auto p-8">
       <h1 className=" text-9xl font-bold mb-4">
         JR
         <span className="bg-black text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500">
           ui
         </span>
       </h1>
+      <p>Cumming Soon!</p>
     </div>
   );
 }
